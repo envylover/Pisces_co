3d7808578da1d5779ec92e660434500b6ba46557
ming he <heming12138@outlook.com>
2021-10-02 19:57:54 +0800

    10/2

--- core/co.hpp
+++ core/co.hpp
@@ -1,12 +1,81 @@
 #pragma once
 
 #include <coroutine>
-#include <string>
-#include <functional>
+
 #include <tuple>
 #include <concepts>
-#include <memory>
-#include "p_basis.h"
+
+
+namespace pisces
+{
+
+	struct emptyDerived {};
+
+	using namespace std;
+	template<typename T>
+	concept exist_await_ready = requires (T && t) {
+		t.ready();
+		requires same_as<decltype(t.ready()), bool>;
+	};
+
+	template<typename T, typename promise_type = void>
+	concept exist_await_suspend = requires (T && t, std::coroutine_handle<promise_type> h) {
+		t.suspend(h);
+		requires same_as<decltype(t.suspend(h)), void>;
+	};
+
+	template<typename T>
+	concept exist_await_resume = requires (T && t) {
+		t.resume();
+		//requires same_as<decltype(t.resume()), void>;
+	};
+
+	template<class Ty>
+	concept default_initialize = requires (Ty && t) {
+		t.co_init();
+	};
+	template<class Ty>
+	concept default_final = requires (Ty && t) {
+		t.co_final();
+	};
+
+	template<class Ty>
+	concept default_except = requires (Ty && t) {
+		t.co_except();
+	};
+
+	struct co_tag_t {};
+
+	//-------------------------------------------------------------
+	struct initial_suspend_always_tag :co_tag_t {};
+	struct initial_suspend_never_tag :co_tag_t {};
+	struct final_suspend_never_tag :co_tag_t {};
+	struct final_suspend_always_tag :co_tag_t {};
+	struct yield_value_always_tag :co_tag_t {};
+	struct yield_value_never_tag :co_tag_t {};
+	//-------------------------------------------------------------
+
+
+	template<typename...tags>
+	struct co_tag :tags...{};
+
+	template<>
+	struct co_tag<> :initial_suspend_always_tag, final_suspend_always_tag, yield_value_always_tag {};
+
+	template<typename tag>
+	constexpr bool mutex_tag_v = (derived_from<tag, initial_suspend_always_tag>
+		&& derived_from<tag, initial_suspend_never_tag>)
+		|| (derived_from<tag, final_suspend_never_tag>
+			&& derived_from<tag, final_suspend_always_tag>)
+		|| (derived_from<tag, yield_value_always_tag>
+			&& derived_from<tag, yield_value_never_tag>);
+
+
+	template<typename tag>
+	concept RightTag = !mutex_tag_v<tag>;
+}
+
+
 namespace pisces
 {
 	using namespace std;
@@ -17,15 +86,31 @@ namespace pisces
 	    typename Co_tag = co_tag<>
 	>
 	requires RightTag<Co_tag> && default_initializable<emptyDerived>
+
 	class task
 	{
 		Derived* pDeri = nullptr;
 	public:
 		struct promise_type;
-		explicit task(std::coroutine_handle<promise_type> h):_handler(h) {
+		explicit task(std::coroutine_handle<promise_type>& h):_handler(h) {
 			if constexpr (same_as<Derived, emptyDerived>);
 			else
 				pDeri = static_cast<Derived*>(this);
+		}
+
+		explicit task(std::coroutine_handle<promise_type>&& h) :_handler(h) {
+			if constexpr (same_as<Derived, emptyDerived>);
+			else
+				pDeri = static_cast<Derived*>(this);
+		}
+
+		task(task& other)
+		{
+			task(other._handler);
+		}
+		task(task&& other)
+		{
+			task(std::move(other._handler));
 		}
 		task() {}
 	    //----------------------------------------------------
@@ -34,6 +119,7 @@ namespace pisces
 			pDeri = nullptr;
 			if (_handler)
 				_handler.destroy();
+			_handler = nullptr;
 		}
 
 	public:
@@ -71,7 +157,15 @@ namespace pisces
 				else
 					return suspend_never{};
 			}
+			
 			auto yield_value(RTY&& val) {
+				_res = val;
+				if constexpr (derived_from<Co_tag, yield_value_never_tag>)
+					return suspend_never{};
+				else
+					return suspend_always();
+			}
+			auto yield_value(RTY& val) {
 				_res = val;
 				if constexpr (derived_from<Co_tag, yield_value_never_tag>)
 					return suspend_never{};
@@ -94,7 +188,24 @@ namespace pisces
 			}
 			decay_t<RTY>                            _res;
 		};
-		
+		void* address()
+		{
+			if (_handler)
+				return _handler.address();
+			return nullptr;
+		}
+
+		void from_address(void* addr)
+		{
+			if (addr)
+			{
+				if (_handler)
+					_handler.destroy();
+				_handler = _handler.from_address(addr);
+			}
+
+		}
+
 		RTY get()
 		{
 			return _handler.promise()._res;
@@ -110,6 +221,7 @@ namespace pisces
 		bool done() {
 			if (_handler)
 				return _handler.done();
+			return true;
 		}
 		void destory()
 		{
@@ -118,6 +230,9 @@ namespace pisces
 			_handler = nullptr;
 			return;
 		}
+
+
+	private:
 		std::coroutine_handle<promise_type> _handler;
 
 
